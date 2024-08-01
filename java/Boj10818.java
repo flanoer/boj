024f93c95a4cf815f1ba4fd1cad1abec0cc34a64
Kiwoong Nam <36145855+flanoer@users.noreply.github.com>
2024-08-01 19:09:59 +0900

    boj10818 풀이(메모리 사용량이 큼, 리팩토링 필요!)

--- java/Boj10818.java
+++ java/Boj10818.java
@@ -17,14 +17,34 @@ public class Boj10818 {
 
         int n = in.nextInt();
         int loop = 0;
-        int array = new int[n];
-        do {
-            String intData = in.next();
-            array[loop] = Integer.parseInt(intData);
+        int[] array = new int[n];
+
+        while(loop < n) {
+            array[loop] = Integer.parseInt(in.next());
             loop++;
-        } while(loop < n);
+        }
 
-        // 정렬인가?
+        int min = 0;
+        int max = 0;
+        for(int i = 0 ; i < n ; i++) {
+            int next = i+1;
+            if(i == 0) {
+                min = array[i];
+                max = array[i];
+            }
 
+            if(next == n) break;
+
+            if(array[next] < min) {
+                min = array[next];
+                continue;
+            }
+
+            if(array[next] > max) {
+                max = array[next];
+            }
+        }
+
+        System.out.printf("%d %d", min, max);
     }
 }
